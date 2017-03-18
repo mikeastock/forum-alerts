34e96d61103261249bff443c2c8a190a82db8969
Michael Stock <mikeastock@gmail.com>
2017-03-17 18:42:55 -0700

    Pattern match for success

--- lib/forum_alerts/notifier.ex
+++ lib/forum_alerts/notifier.ex
@@ -2,6 +2,6 @@ defmodule ForumAlerts.Notifier do
   @channel System.get_env("SLACK_CHANNEL")
 
   def notify(message) do
-    Slack.Web.Chat.post_message(@channel, "```#{message}```")
+    %{"ok" => true} = Slack.Web.Chat.post_message(@channel, "```#{message}```")
   end
 end
